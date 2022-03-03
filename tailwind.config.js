7943047e87bd1335a097bf68dcdcdeb46c46ddb1
KHazen1 <98141017+KHazen1@users.noreply.github.com>
2022-03-03 12:48:49 -0600

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './index.html', './src/**/*.{vue,js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {
+      minHeight: {
+        "screen-nonav": "calc(100vh - 88px)",
+      },
+    },
+  },
+  plugins: [],
+}
